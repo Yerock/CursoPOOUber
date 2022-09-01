ac9bb31fec609e45d184d2ebcde6c70a43d249d0
Yerol Gualdron <yerol9520@gmail.com>
2022-09-01 18:28:57 -0500

    Modificamos la clase Car.java y la clase main.java

--- Java/Car.java
+++ Java/Car.java
@@ -2,14 +2,16 @@ class Car {
     Integer id;
     String license;
     Account driver;
-    Integer passegenger;
+    private Integer passegenger;
     
     public Car(String license, Account driver){
         this.license = license;
         this.driver = driver;
+        passegenger = 3;
+        System.out.println("passenger:" + passegenger);
     }
 
     void printeDataCar() {
-        System.out.println("License: " + license + "Driver" + driver.name);
+        System.out.println("License: " + license + "Driver" + driver.name + " Passengers: " + passegenger);
     }
 }
--- Java/Main.java
+++ Java/Main.java
@@ -1,14 +1,14 @@
 class Main {
 public static void main(String[] args) {
    System.out.println("Hola Mundo")
-   Car car = new Car("AMQ123", new Account("Andres Herrera", "AND123"));
+   UberX uberX = new UberX("AMQ123", new Account("Andres Herrera", "AND123"), "Chevrolet", "Sonic");
    
-   car.passegenger = 4;
-   car.printeDataCar();
+   uberX.passegenger = 3;
+   uberX.printeDataCar();
 
-   Car car2 = new Car("QWE576", new Accountt("Andrea Herrera", "ANDA876"));
+   /*Car car2 = new Car("QWE576", new Accountt("Andrea Herrera", "ANDA876"));
    
    car2.passegenger = 3;
    car2.printeDataCar();
-
+*/
 }
